c80c84c13679c6344d13ed726fcbe6fcfebd09dd
junnnhhh <imk1017@naver.com>
2024-02-01 23:39:27 +0900

    memo 로직 정리

--- init.js
+++ init.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const ejs = require('ejs');
 
+const memoRouter = require('./memo/memo.js');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+app.use('/memo', memoRouter);
+
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/memo/ejs' + '/memo_list.ejs');
+});
 
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), 'port is running...');
 });
-
-module.exports = app;
---
+++ memo/memo.js
@@ -0,0 +1,61 @@
+const pool = require('../db_connection.js');
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const conn = await pool.getConnection();
+
+    const rows = await conn.query('SELECT * FROM junsware.memo');
+
+    res.send(rows);
+
+    conn.release();
+});
+
+router.post('/', async (req, res) => {
+    const conn = await pool.getConnection();
+    
+    const add = await conn.query(
+        "INSERT INTO junsware.memo (title, body)"
+        + " value (?, ?)", 
+        [req.body.title, req.body.form]);
+    
+    res.send("success");
+
+    conn.release();
+});
+
+router.get('/create', (req, res) => {
+    res.render(__dirname + '/ejs/create-memo');
+});
+
+router.get('/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+
+    var id = req.params.id;
+
+    const data = await conn.query(
+        'SELECT seq, title, body FROM junsware.memo WHERE seq = ?'
+    , [id]);
+    
+    res.render(__dirname + '/ejs/memo', data[0]);
+
+    conn.release();
+});
+
+router.delete('/:id', async (req, res) => {
+    const conn = await pool.getConnection();
+
+    var id = req.params.id;
+
+    const data = await conn.query(
+        'DELETE FROM junsware.memo WHERE seq = ?', [id]
+    );
+
+    res.send('success');
+
+    conn.release();
+});
+
+module.exports = router;
