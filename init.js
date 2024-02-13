28b3d50bf0eb5a31e9b9f60851c312967bb7a8d8
junnnhhh <imk1017@naver.com>
2024-02-13 23:25:52 +0900

    로그인 기능 구현 ~ing

--- init.js
+++ init.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const loginRouter = require('./module/user/login.js');
 const memoRouter = require('./module/memo/memo.js');
 
 const app = express();
@@ -8,12 +9,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+app.use(function(req, res, next) {
+    const originUrl = req.originalUrl;
+
+    if(originUrl == '/login') {
+        next();
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.use('/login', loginRouter);
 app.use('/memo', memoRouter);
+
+
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.redirect('/memo/list');
+    res.redirect('/login');
 });
 
 app.set('port', process.env.PORT || 3000);
